96b78c75784135f3cc1a6dc8f0753af64f6f01a7
Josh Pratt <josh@learnatvivid.com>
2016-12-12 08:32:33 -0800

    Gulp file updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulf-if');
 
-gulp.task('hello', function() {
-  console.log('branch test!');
-
+//SASS processing
+gulp.task('sass', function(){
+  return gulp.src('site/assets/scss/**/*.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('site/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('sass', function(){
-  return gulp.src('site/assets/css/styles.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('dist/assets/css'))
+//watch function
+gulp.task('watch', ['browserSync', 'sass'], function() {
+  gulp.watch('site/assets/scss/**/*.scss', ['sass']);
+  gulp.watch('site/assets/images/**/*.+(png|jpg|jpeg|gif|svg)', browserSync.reload);
+  gulp.watch('site/**/*.html', browserSync.reload);
+  gulp.watch('site/assets/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'site'
+    },
+  })
+});
+
+gulp.task('useref', function() {
+  return gulp.src('site/*.html')
+    .pipe(useref())
+    .pipe(gulp.dest('dist'))
 
 });
